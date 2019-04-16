7a58d41a03895fd6913c638c53b43838b0749718
Katarzyna Berbeka <katarzyna.berbeka@creativestyle.pl>
2019-04-16 22:57:42 +0200

    Add maschroom with dots

--- sketch.js
+++ sketch.js
@@ -125,3 +125,85 @@ var s2 = function(sketch) {
 };
 
 var myp2 = new p5(s2, 'dots');
+
+//
+// muschroom
+//
+
+var s3 = function(sketch) {
+  var width = 300;
+  var height = 300;
+  var canvas1;
+  var song;
+  var sliderVolume;
+  var sliderRate;
+  var sliderPan;
+  var button;
+  var amp;
+  var volHistory = [];
+
+  sketch.setup = function() {
+    canvas1 = sketch.createCanvas(width, height);
+    sliderVolume = sketch.createSlider(0, 1, 0.5, 0.01);
+    sliderRate = sketch.createSlider(0.5, 1, 0.75, 0.01);
+    sliderPan = sketch.createSlider(-1, 1, 1, 0.01);
+    song = sketch.loadSound('charlotte.mp3', loaded);
+    amp = new p5.Amplitude();
+  };
+
+  function loaded() {
+    console.log('loaded');
+    button = sketch.createButton('Play');
+    button.mousePressed(togglePlaying);
+  }
+
+  function togglePlaying() {
+    if (!song.isPlaying()) {
+      song.play();
+      song.setVolume(0.3);
+      button.html('Pause');
+    } else {
+      song.pause();
+      button.html('Play');
+    }
+  }
+
+  sketch.draw = function() {
+    sketch.background(51);
+    var vol = amp.getLevel();
+    var diam = sketch.map(vol, 0, 0.3, 10, 50);
+    // sketch.ellipse(300 / 2, 300 / 2, diam, diam);
+    sketch.stroke(255);
+    sketch.noFill();
+    //mashroom
+    sketch.beginShape((base = 180), (x = diam), (a = base + x), (b = base - x));
+    sketch.arc(150, 100, a, b, sketch.PI, sketch.TWO_PI);
+    //hat bottom
+    sketch.vertex(150 - a / 2, 100);
+    //leg
+    sketch.vertex(130, 100);
+    sketch.vertex(130, 180);
+    sketch.vertex(130, a);
+    //basement
+    sketch.vertex(170, a);
+    //leg - moving
+    sketch.vertex(170, b);
+    //leg
+    sketch.vertex(170, 100);
+    sketch.vertex(150 + a / 2, 100);
+    sketch.endShape();
+
+    //mashroom dots
+    sketch.fill(diam, diam, diam);
+    sketch.ellipse(90, 60, 20, 20);
+    sketch.ellipse(130, 60, 20, 20);
+    sketch.ellipse(210, 80, 30, 30);
+
+    // sliders
+    song.setVolume(sliderVolume.value());
+    song.pan(sliderRate.value());
+    song.rate(sliderPan.value());
+  };
+};
+
+var myp3 = new p5(s3, 'maschroom');
