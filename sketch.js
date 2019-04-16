9e23f4059fa87eebecfae42221de43789e42a9d5
Katarzyna Berbeka <katarzyna.berbeka@creativestyle.pl>
2019-04-16 18:13:17 +0200

    17.4: Amplitude Analysis

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ var sliderRate;
 var sliderPan;
 var button;
 var jumpButton;
+var amp;
 
 function setup() {
   createCanvas(300, 300);
@@ -11,15 +12,15 @@ function setup() {
   sliderRate = createSlider(0.5, 1, 0.75, 0.01);
   sliderPan = createSlider(-1, 1, 1, 0.01);
   song = loadSound('charlotte.mp3', loaded);
+  amp = new p5.Amplitude();
+}
+
+function loaded() {
+  console.log('loaded');
   button = createButton('Play');
   button.mousePressed(togglePlaying);
   jumpButton = createButton('Jump');
   jumpButton.mousePressed(jumpSong);
-  song.addCue(3, changeBackground);
-}
-
-function changeBackground() {
-  background(random(255), random(255), random(255));
 }
 
 function togglePlaying() {
@@ -37,42 +38,17 @@ function jumpSong() {
   var len = song.duration();
   var t = 0; // random(len);
   song.jump(t);
-  console.log(t);
-}
-
-function loaded() {
-  console.log('loaded');
 }
 
 function draw() {
-  // background(0);
+  background(51);
+  var vol = amp.getLevel();
+  var diam = map(vol, 0, 0.3, 10, 200);
+  fill(100, 15, 100);
+  ellipse(300 / 2, 300 / 2, diam, diam);
+
+  // sliders
   song.setVolume(sliderVolume.value());
   song.pan(sliderRate.value());
   song.rate(sliderPan.value());
-
-  // if (song.currentTime() > 5) {
-  //   background(255, 0, 255);
-  // }
-
-  //background(song.currentTime() * 10, 0, 255);
 }
-
-//preload
-
-// var song;
-// var slider;
-
-// function preload() {
-//   song = loadSound('traveller.mp3');
-// }
-
-// function setup() {
-//   createCanvas(400, 400);
-//   slider = createSlider(0, 1, 0.5, 0.01);
-//   song.play();
-// }
-
-// function draw() {
-//   background(0);
-//   song.setVolume(slider.value());
-// }
